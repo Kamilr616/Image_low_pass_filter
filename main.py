218090afe2927401ff2194cdf298a7dc45a87a6c
Kamil <kamilr616@gmail.com>
2023-03-23 16:24:55 +0100

    add noise

--- main.py
+++ main.py
@@ -5,29 +5,36 @@ from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
 
 
-def select_image():
+#TODO image_save
+def select_image(flag):
     global img, img_new
-    path.set(askopenfilename())
+    if flag == 1:
+        path.set(askopenfilename())
+    else:
+        path.set("images/lena.bmp")
     if len(path.get()) > 0:
         img = cv2.imread(path.get())
         img_new = img
     assert img is not None, "file could not be read, check with os.path.exists()"
+    #show_image()
+    start()
 
-
+#TODO show_mask
 def show_image():
     # grab a reference to the image panels
     global panelA, panelB, img, img_new
     # OpenCV represents images in BGR order; however PIL represents
     # images in RGB order, so we need to swap the channels
     img_show = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_new_show = cv2.cvtColor(img_new, cv2.COLOR_BGR2RGB)
+
     # convert the images to PIL format...
     img_show = Image.fromarray(img_show)
-    img_new_show = Image.fromarray(img_new)
+    img_new_show = Image.fromarray(img_new_show)
     # ...and then to ImageTk format
     img_show = ImageTk.PhotoImage(img_show)
     img_new_show = ImageTk.PhotoImage(img_new_show)
     # if the panels are None, initialize them
-    #TODO code below to main
     if panelA is None or panelB is None:
         # the first panel will store our original image
         panelA = Label(image=img_show)
@@ -71,6 +78,15 @@ def image_swap():
     img, img_new = img_new, img
     show_image()
 
+#TODO image noise func
+def image_noise():
+    global img
+    gauss_noise = numpy.zeros(img.shape, dtype=numpy.uint8)
+    cv2.randn(gauss_noise, 128, 20)
+    gauss_noise = (gauss_noise * 0.5).astype(numpy.uint8)
+    img = cv2.add(img, gauss_noise)
+    show_image()
+
 
 # initialize the window toolkit along with the two image panels
 root = Tk()
@@ -81,7 +97,6 @@ img = None
 img_new = None
 # file path
 path = StringVar()
-#TODO default path and show
 
 # Mask type
 mask_type = IntVar()
@@ -125,13 +140,17 @@ for size, val in sizes:
                 padx=20,
                 variable=mask_size,
                 value=val).pack(anchor=W)
+select_image(0)
 
 # create a button,then add the button the GUI
-btn = Button(root, text="Select an image", command=select_image)
+btn = Button(root, text="Select an image", command=lambda: select_image(1))
 btn.pack(side="bottom", fill="both", expand=1, padx="10", pady="10")
 btn1 = Button(root, text="Filter", command=start)
 btn1.pack(side="bottom", fill="both", expand=1, padx="10", pady="10")
-btn1 = Button(root, text="Swap images", command=image_swap)
-btn1.pack(side="bottom", fill="both", expand=1, padx="10", pady="10")
+btn2 = Button(root, text="Swap images", command=image_swap)
+btn2.pack(side="bottom", fill="both", expand=1, padx="10", pady="10")
+btn3 = Button(root, text="Add noise", command=image_noise)
+btn3.pack(side="bottom", fill="both", expand=1, padx="10", pady="10")
+
 # kick off the GUI
 root.mainloop()
