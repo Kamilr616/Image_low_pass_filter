2916a6262ec33c4f823dd86a77fe88b590c8f445
Kamil <kamilr616@gmail.com>
2023-03-26 21:53:41 +0200

    espanol and ukrainian lang

--- main.py
+++ main.py
@@ -83,10 +83,10 @@ def start():
     m_type = mask_type.get()
 
     if m_type == 102:
-        kernel = numpy.ones((m_size, m_size), numpy.uint8) / m_size ** 2
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (m_size, m_size))
         kernel = kernel / numpy.sum(kernel)
     if m_type == 105:
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (m_size, m_size))
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (m_size, m_size))
         kernel = kernel / numpy.sum(kernel)
     elif m_type == 106:
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (m_size, m_size))
@@ -117,7 +117,7 @@ def start():
 
 def image_swap():
     global img, img_new
-    img, img_new = img_new, img
+    img = img_new
     show_image()
 
 
@@ -134,28 +134,74 @@ def image_noise():
 
 
 def change_language():
-    ger = {
-        "l1": "Wählen Sie den Kernel-Typ:",
-        "l2": "Wählen Sie die Kernel-Größe:",
-        "l3": "Wählen Sie die Sprache:",
+    ukr = {
+        "l1": "Виберіть тип ядра:",
+        "l2": "Виберіть розмір ядра:",
+        "l3": "Виберіть мову:",
+
+        "k1": "Ядро:",
+        "k2": "Вхід:",
+        "k3": "Вихід:",
+
+        "m1": "Середнє",
+        "m2": "Гауссівське",
+        "m3": "Перехресне",
+        "m4": "Еліпс",
+        "m5": "Трапеція",
+        "m6": "Медіанне",
+        "m7": "Білатеральне",
+
+        "b0": "Вибрати зображення",
+        "b1": "Зберегти зображення як",
+        "b2": "Поміняти зображення",
+        "b5": "Додати шум",
+    }
+    esp = {
+        "l1": "Seleccione el tipo de kernel:",
+        "l2": "Seleccione el tamaño del kernel:",
+        "l3": "Seleccione el idioma:",
 
         "k1": "Kernel:",
-        "k2": "Input:",
-        "k3": "Output:",
+        "k2": "Entrada:",
+        "k3": "Salida:",
+
+        "m1": "Promedio",
+        "m2": "Gaussiano",
+        "m3": "Cruzado",
+        "m4": "Eclipse",
+        "m5": "Trapecio",
+        "m6": "Mediana",
+        "m7": "Bilateral",
+
+        "b0": "Seleccionar imagen",
+        "b1": "Guardar imagen como",
+        "b2": "Intercambiar imagen",
+        "b5": "Agregar ruido",
+    }
+
+    ger = {
+        "l1": "Wähle Kerntyp:",
+        "l2": "Wähle Kerngröße:",
+        "l3": "Wähle Sprache:",
+
+        "k1": "Kern:",
+        "k2": "Eingabe:",
+        "k3": "Ausgabe:",
 
         "m1": "Durchschnitt",
-        "m2": "Gaussian",
-        "m3": "Rechteck",
-        "m4": "Ellipse",
+        "m2": "Gauß",
+        "m3": "Kreuz",
+        "m4": "Eclipse",
         "m5": "Trapezoid",
         "m6": "Median",
         "m7": "Bilateral",
 
-        "b0": "Wählen Sie ein Bild",
-        "b1": "Bild speichern als",
-        "b2": "Bilder tauschen",
+        "b0": "Bild auswählen",
+        "b1": "Bild speichern unter",
+        "b2": "Bild austauschen",
         "b5": "Rauschen hinzufügen",
     }
+
     pol = {
         "l1": "Wybierz typ maski:",
         "l2": "Wybierz rozmiar maski:",
@@ -167,15 +213,15 @@ def change_language():
 
         "m1": "Średnia",
         "m2": "Gaussowska",
-        "m3": "Prostokątna",
+        "m3": "Krzyżowa",
         "m4": "Elipsa",
-        "m5": "Trapezoidalna",
+        "m5": "Trapezowa",
         "m6": "Medianowa",
         "m7": "Bilateralna",
 
         "b0": "Wybierz obraz",
         "b1": "Zapisz obraz jako",
-        "b2": "Zamień obrazy",
+        "b2": "Zamień obraz",
         "b5": "Dodaj szum",
     }
     eng = {
@@ -189,7 +235,7 @@ def change_language():
 
         "m1": "Average",
         "m2": "Gaussian",
-        "m3": "Rectangle",
+        "m3": "Cross",
         "m4": "Eclipse",
         "m5": "Trapezoid",
         "m6": "Median",
@@ -197,7 +243,7 @@ def change_language():
 
         "b0": "Select image",
         "b1": "Save image as",
-        "b2": "Swap images",
+        "b2": "Swap image",
         "b5": "Add noise",
     }
     typed = lang.get()
@@ -207,6 +253,10 @@ def change_language():
         dict1 = pol
     elif typed == 203:
         dict1 = ger
+    elif typed == 204:
+        dict1 = esp
+    elif typed == 205:
+        dict1 = ukr
     else:
         dict1 = eng
 
@@ -238,7 +288,6 @@ root.title("Low pass filter")
 ico = Image.open('images/ans.jpeg')
 photo = ImageTk.PhotoImage(ico)
 root.wm_iconphoto(False, photo)
-
 
 panelA = None
 panelB = None
@@ -295,7 +344,9 @@ buttons = [(lambda: select_image(1), b0),
 
 langs = [("English", 201, lambda: change_language()),
          ("Polski", 202, lambda: change_language()),
-         ("Deutsch", 203, lambda: change_language())]
+         ("Deutsch", 203, lambda: change_language()),
+         ("Español", 204, lambda: change_language()),
+         ("Українська", 205, lambda: change_language())]
 
 mask_size.set(3)
 mask_size.set(5)
@@ -329,7 +380,7 @@ for comm, b in buttons:
            activeforeground="#2196F3",
            command=comm).pack(side="left", fill="both", expand=1, padx="10", pady="10")
 
-w1 = Scale(frame1, from_=0, resolution=0.1, to=10, orient="horizontal", foreground="#2196F3")
+w1 = Scale(frame1, from_=0, resolution=0.1, to=10, orient="horizontal", background="#1976D2")
 w1.pack(side="left", fill="both", expand=1, ipadx="40", pady="10")
 w1.set(0.5)
 
